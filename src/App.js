23f80c6326bc22777bd1cd1606f99f179cde1434
ragibaunjum01 <ragibhasan01@gmail.com>
2021-02-23 03:31:22 +0600

    react hero

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <h1>My first react app</h1>
+        <Person name = 'Shakib Al Hasan' catagory = 'Best all-rounder'></Person>
+        <Person name = 'Tamim Iqbal' catagory = 'Best opener'></Person>
+        <Person name = 'Mushfiqur Rahim' catagory = 'Best wicket-keeper'></Person>
       </header>
+      
     </div>
   );
+}
+function Person(props){
+  const personStyle = {
+    border:'2px solid white',
+    margin:'10px',
+    padding:'10px',
+    width: '300px'
+  }
+  return (
+    <div  style={personStyle}>
+      <h2>{props.name}</h2>
+      <h4>{props.catagory}</h4>
+    </div>
+  )
 }
 
 export default App;
